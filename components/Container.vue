6ae553bb2dcfe4604f51834a19c92dbad85dbc73
signofcommunication <jhvonsky@gmail.com>
2024-11-12 20:05:56 +0700

    styling: update styling

--- components/Container.vue
+++ components/Container.vue
@@ -61,24 +61,23 @@ async function getPokemonByName() {
 
 <template>
   <section class="q-pa-md">
-    <div>
-      <label
-        for="small-input"
-        class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-      >
-        Search Pokémon
-      </label>
-      <input
-        type="text"
-        id="small-input"
-        class="block w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-        v-model="search"
-      />
-      <button @click="getPokemonByName">Search</button>
+    <div class="flex items-center justify-center space-x-2 p-4">
+      <div class="w-full max-w-xs">
+        <q-input
+          square
+          outlined
+          v-model="search"
+          label="Search Pokemon"
+          class="w-full"
+        />
+      </div>
+      <q-btn @click="getPokemonByName" color="primary" label="Search" />
     </div>
 
     <!-- Display loader while data is being fetched -->
-    <div v-if="isLoading" class="text-center my-4">Loading...</div>
+    <div v-if="isLoading" class="text-center my-4">
+      <q-spinner-orbit color="primary" size="10em" />
+    </div>
 
     <!-- Display Pokémon list once data is loaded -->
     <div v-else class="flex items-center justify-center">
@@ -104,7 +103,7 @@ async function getPokemonByName() {
 
     <!-- Pagination control -->
     <div class="q-pa-lg flex flex-center">
-      <q-pagination v-model="current" :max="5" />
+      <q-pagination v-model="current" :max="10" />
     </div>
   </section>
 </template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,86 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: [
+    "./components/**/*.{js,vue,ts}",
+    "./pages/**/*.vue",
+    "./plugins/**/*.{js,ts}",
+    "./app.vue",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      fontSize: {
+        "40px": "40px",
+        "8xl": "6rem",
+        "45px": "45px",
+      },
+      colors: {
+        primary: {
+          50: "#eff6ff",
+          100: "#dbeafe",
+          200: "#bfdbfe",
+          300: "#93c5fd",
+          400: "#60a5fa",
+          500: "#3b82f6",
+          600: "#2563eb",
+          700: "#1d4ed8",
+          800: "#1e40af",
+          900: "#1e3a8a",
+          950: "#172554",
+        },
+        "gaws-base-color": "hsl(239, 100%, 35%)",
+        "gaws-secondary-color": "45474B",
+      },
+      fontFamily: {
+        body: [
+          "Inter",
+          "ui-sans-serif",
+          "system-ui",
+          "-apple-system",
+          "system-ui",
+          "Segoe UI",
+          "Roboto",
+          "Helvetica Neue",
+          "Arial",
+          "Noto Sans",
+          "sans-serif",
+          "Apple Color Emoji",
+          "Segoe UI Emoji",
+          "Segoe UI Symbol",
+          "Noto Color Emoji",
+        ],
+        sans: [
+          "Inter",
+          "ui-sans-serif",
+          "system-ui",
+          "-apple-system",
+          "system-ui",
+          "Segoe UI",
+          "Roboto",
+          "Helvetica Neue",
+          "Arial",
+          "Noto Sans",
+          "sans-serif",
+          "Apple Color Emoji",
+          "Segoe UI Emoji",
+          "Segoe UI Symbol",
+          "Noto Color Emoji",
+        ],
+      },
+      animation: {
+        marquee: "marquee 25s linear infinite",
+        marquee2: "marquee2 25s linear infinite",
+      },
+      keyframes: {
+        marquee: {
+          "0%": { transform: "translateX(0%)" },
+          "100%": { transform: "translateX(-100%)" },
+        },
+        marquee2: {
+          "0%": { transform: "translateX(100%)" },
+          "100%": { transform: "translateX(0%)" },
+        },
+      },
+    },
   },
   plugins: [],
 };
